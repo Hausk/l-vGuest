b6e16d628899b6a100060532c7cf7521adc59c0b
Hausk <dbessa.jonathan@gmail.com>
2024-10-17 17:53:03 +0200

    feat(apiCategories): added an api to fetch images in categories.

---
+++ server/api/categories/[slug].get.ts
@@ -0,0 +1,23 @@
+import { eq } from 'drizzle-orm'
+import { db } from '~~/server/db'
+import { categories } from '~~/server/database/schema'
+
+export default eventHandler(async (event: any) => {
+  const slug: string = event.context.params?.slug
+
+  if (!slug) {
+    throw createError({ statusCode: 400, statusMessage: 'Slug is required' })
+  }
+
+  const category = await db.query.categories.findFirst({
+    where: eq(categories.slug, slug),
+    with: {
+      images: true
+    }
+  })
+
+  if (!category) {
+    throw createError({ statusCode: 404, statusMessage: 'Category not found' })
+  }
+  return category
+})
